96d4579d3a05d8614e3841d245cc734efccaf19a
c5231481 <Uladzimir.Malko@sap.com>
2018-04-05 11:38:39 +0300

    implemented base admin panel [Julia]

--- src/app/components/supplier/supplier.component.ts
+++ src/app/components/supplier/supplier.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import {ReactiveFormsBaseClass} from '../shared/classes/reactive-forms.base.class';
+import { ReactiveFormsBaseClass } from '../shared/classes/reactive-forms.base.class';
 
 declare var $: any;
 
